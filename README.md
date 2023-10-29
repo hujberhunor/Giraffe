8155aee3addd0e2a5af045da4320a02e41a27dd7
Lolis4TheWin <hujber.hunor@gmail.com>
2023-10-29 22:39:21 +0100

    It is WORKING!!

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # Giraffe_v2
 
 
-COMPILE
+### COMPILE
 gcc audio.c fileMan.c -o random -lm
 
-TODO
+### TODO
+
+AUDIO.c
 - Debugmalloc avagy memLeak ellenőrzés + hibaüzenetek írása
 - audio.h hibaszűrés
 
-FILEMAN
+FILEMAN.c
   - fileMan line28 fix lista érték javítás? !! NE LEGYEN STATIKUS!! Láncolt lista
   - .wav-ra szűrés !! 
--- audio.c
+++ audio.c
@@ -16,36 +16,7 @@
 #include <stdio.h>
 #include <string.h>
 
-
-int songSelect(char** array, int sizeArray){
-  int selected = 0;
- 
-  /* Print the songs in the list */
-  for(int i = 0; i < sizeArray; i++){
-    printf("%d\t| %s\n",i+1, array[i]);
-  }
-
-  printf("Select a song! ");
-  scanf("%d", &selected);
-
-  return selected-1;
-}
-
-void progesssBar(ma_result soundResult , int lengthSec){
-  int iter = 0;
-  while(soundResult == MA_SUCCESS){
-    //printf("result = %d ", (int) cursor);
-    printf("\r%i/%i",  iter, lengthSec);
-    fflush(stdout); 
-    sleep(1);
-    iter++;
-  }
-}
-/* NEEDS ITS OWN HEADER */
-
-
-
-int main(){
+/* GLOBAL VARIABLES (MiniAudio) (SORRY) */
   // Decoder 
   ma_decoder decoder;
   ma_result resultDecoder;
@@ -59,10 +30,53 @@ int main(){
   ma_resource_manager resourceManager;
   // Audio engine
   ma_uint64 cursor;
-  ma_result resultCursor;
+  ma_uint64 resultCursor;
   ma_engine engine;
   ma_result resultSound;
+
+
+
+
+/*NEEDS ITS OWN HEADER FILE */
+int songSelect(char** array, int sizeArray){
+  int selected = 0;
  
+  /* Print the songs in the list */
+  for(int i = 0; i < sizeArray; i++){
+    printf("%d\t| %s\n",i+1, array[i]);
+  }
+
+  printf("Select a song! ");
+  scanf("%d", &selected);
+
+  return selected-1;
+}
+/* RETURNS THE VALUE OF THE READ FILE LENGTH IN SEC */
+int lenInSec(){
+  resultFrameSec = ma_data_source_get_length_in_pcm_frames(&decoder, &length);
+  if (resultFrameSec != MA_SUCCESS) {return result;} 
+  return (int) length/(float)decoder.outputSampleRate;
+}
+
+/* PROGRESS "BAR" */
+void progesssBar(ma_result soundResult){
+  int iter = 0;
+  int lengthSec = lenInSec();
+  
+  while(soundResult == MA_SUCCESS){
+    //printf("result = %d ", (int) cursor);
+    printf("\r%i/%i", iter, lenInSec());
+    fflush(stdout); 
+    sleep(1);
+    iter++;
+  }
+}
+
+/* NEEDS ITS OWN HEADER */
+
+
+
+int main(){
   /* from fileMan */
   char* dir = "./songs/";
   int songArraySize;                               // size of the songList 
@@ -84,25 +98,17 @@ int main(){
   resultRm = ma_resource_manager_init(&config, &resourceManager);
   if (resultRm != MA_SUCCESS) {printf("ResourceManager error");return -1;}  
 
-  /* Song duration in sec */
-  resultFrameSec = ma_data_source_get_length_in_pcm_frames(&decoder, &length);
-  if (resultFrameSec != MA_SUCCESS) {return result;} 
-  int lenInSec = (int) length/(float)decoder.outputSampleRate;
-
   /* Sound engine  */
   resultSound = ma_engine_init(NULL, &engine);
   if (resultSound != MA_SUCCESS) {printf("Engine error");return -1;} 
 
   ma_engine_play_sound(&engine, fp, NULL);
-  
-  /* Progress bar */
-  resultCursor = ma_data_source_get_cursor_in_pcm_frames(&decoder, &cursor);
-  if (resultCursor != MA_SUCCESS) return result;
-  
+
+/* IMPLEMENTÁLNI KELL A PROGRESS BAR FUNCTIONBA!!  */
   if(resultSound == MA_SUCCESS) printf("Currently playing: %s \n", fp);
-  progesssBar(resultSound ,lenInSec);
+  progesssBar(resultSound);
 
-
+  /* MiniAudio CLEANUP */
   ma_decoder_uninit(&decoder);
   ma_engine_uninit(&engine);
 }
